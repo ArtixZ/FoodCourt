cabc9df7f0bb5472cba141cd365347691867250c
ArtixZ <artixzhu@gmail.com>
2017-06-28 23:34:04 -0400

    First commit

---
+++ index.ios.js
@@ -0,0 +1,12 @@
+import {
+  AppRegistry,
+  Text
+} from 'react-native';
+// import App from './src/App';
+import React from 'react';
+
+const app = () => (
+    <Text>123</Text>
+);
+
+AppRegistry.registerComponent('manager', () => app);
